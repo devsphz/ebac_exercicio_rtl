0c8337d3635f94475f2be5acf29409688bf53eaa
Pedro Henrique <phenriquesz.1000@gmail.com>
2025-05-26 17:59:58 -0300

    Implementa teste de comentários com sucesso

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,17 +1,18 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import PostComments from './PostComments';
+import Post from '.';
+import PostComment from '.';
 
 describe('Testes do PostComments', () => {
   it('Adiciona 2 comentários', () => {
-    render(<PostComments />);
+    render(<Post />);  // Use o componente com o nome correto
     const input = screen.getByTestId('comment-input');
     const button = screen.getByTestId('submit-button');
 
     fireEvent.change(input, { target: { value: 'Comentário 1' } });
-    fireEvent.click(button); // primeiro comentário
+    fireEvent.click(button);
 
     fireEvent.change(input, { target: { value: 'Comentário 2' } });
-    fireEvent.click(button); // segundo comentário
+    fireEvent.click(button);
 
     expect(screen.getByText('Comentário 1')).toBeInTheDocument();
     expect(screen.getByText('Comentário 2')).toBeInTheDocument();
