71a57bb14a5ddae61dc5f42487b9e0dbf40a5f9b
Pedro Henrique <phenriquesz.1000@gmail.com>
2025-05-26 17:29:47 -0300

    test commit fork

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,21 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import PostComments from './PostComments';
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+describe('Testes do PostComments', () => {
+  it('Adiciona 2 comentários', () => {
+    render(<PostComments />);
+    const input = screen.getByTestId('comment-input');
+    const button = screen.getByTestId('submit-button');
+
+    // Primeiro comentário
+    fireEvent.change(input, { target: { value: 'Comentário 1' } });
+    fireEvent.click(button);
+
+    // Segundo comentário
+    fireEvent.change(input, { target: { value: 'Comentário 2' } });
+    fireEvent.click(button);
+
+    expect(screen.getByText('Comentário 1')).toBeInTheDocument();
+    expect(screen.getByText('Comentário 2')).toBeInTheDocument();
+  });
 });
